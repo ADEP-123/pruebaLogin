5261e18b1290956ac8a245a819f3498d9841c371
ADEP-123 <andep210399@gmail.com>
2023-09-07 07:15:03 -0500

    feat: :sparkles: enrutador

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { getInitRoute } from "./getDataRoutes.js";
 
 
 const initApiRoutes = () => {
     const router = Router();
-    router.use("/login", (req, res, next) => {
-        res.send("hola putos")
-    })
+    // router.use("/login", (req, res, next) => {
+    //     res.send("hola putos")
+    // })
+    router.use("/get", getInitRoute())
+    return router
 }
 
 export default initApiRoutes
