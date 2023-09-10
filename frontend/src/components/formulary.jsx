5d5544ef92d1206229e15b5d755803b3ff917cde
ADEP-123 <andep210399@gmail.com>
2023-09-10 10:26:16 -0500

    feat: :sparkles: formulario login

--- frontend/src/components/formulary.jsx
+++ frontend/src/components/formulary.jsx
@@ -1,38 +1,35 @@
 import React, {useState,useEffect} from "react";
-import PropTypes from 'prop-types'
 
-export default function Formulary({nombre}){
-    const [nom,setNombre] = useState(nombre);
-    const user = "admin";
-    const pass = "contraseña123"
-     const cambiar = async()=>{
-         let res = await(await fetch(`http://127.10.10.10:5010/campus/get/usuarios?user=${user}&pass=${pass}`,{
-            method: "GET",
-
-         })).json();
-         setNombre(JSON.stringify(res))
-        //  console.log(res);
-     }
-
-    // useEffect(()=>{
-    //     let btn = document.querySelector("#info");
-    //     console.log(btn.textContent);
-    // })
-
+export default function Formulary(){
+    async function extractInfo(event){
+        const info = new FormData(event.target);
+        const infoLogin = Object.fromEntries(info);
+        console.log(infoLogin);
+        try {
+            const res = await(await fetch(`http://127.10.10.10:5010/campus/get/usuarios?user=${infoLogin.user_name}&pass=${infoLogin.user_pass}`,{
+               method: "GET",
+            })).json();
+            const token = await res.json()
+            localStorage.setItem('token', token.token );
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    
     return(
         <>
-            <div>formulary
-                <form action="">
+            <div><h1>Formulary</h1>
+                <form onSubmit={extractInfo}>
                     <ul>
                         <li>
                             <label htmlFor="name">Nombre</label>
-                            <input type="text" id="name" name="user_name" placeholder={"user name"} />
+                            <input type="text" id="name" name="user_name" placeholder="user name" />
                         </li>
                     </ul>
                     <ul>
                         <li>
                             <label htmlFor="pass">Contraseña</label>
-                            <input type="text" id="pass" name="user_pass" />
+                            <input type="text" id="pass" name="user_pass" placeholder="password" />
                         </li>
                     </ul>
                     <ul>
@@ -42,19 +39,7 @@ export default function Formulary({nombre}){
                     </ul>
                 </form>
             </div>
-            <button onClick={cambiar}>Click</button>
-            <div id="info">
-                Nombre : {nom}
-            </div>
         </>
         )
 }
 
-//schema
-Formulary.propTypes={
-    nombre: PropTypes.string.isRequired
-}
-//props, son como las propiedades del tag
-Formulary.defaultProps= {
-    nombre: "parangaricutirimicuaro"
-}
