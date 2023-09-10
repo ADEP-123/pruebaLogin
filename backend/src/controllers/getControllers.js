75a7a55c891636cb213ae6e16c5d4b593465fed7
ADEP-123 <andep210399@gmail.com>
2023-09-10 10:38:28 -0500

    feat: :sparkles: arreglando controlador

--- backend/src/controllers/getControllers.js
+++ backend/src/controllers/getControllers.js
@@ -1,24 +1,11 @@
-import 'reflect-metadata';
-import dotenv from 'dotenv';
-import { Router } from 'express';
-import { SignJWT } from 'jose';
-
 import { getRolService } from "../services/getServices.js";
 
 export const getRolController = async (req, res, next) => {
-    const { user, pass } = req.query;
+    const { user, pass} = req.data.payload;
     try {
         const result = await getRolService(user, pass);
         if (result.length != 0) {
-            const encoder = new TextEncoder();
-            const jwtconstructor = new SignJWT(Object.assign({}, Object.assign(result[0])));
-            const jwt = await jwtconstructor
-                .setProtectedHeader({ alg: "HS256", typ: "JWT" })
-                .setIssuedAt()
-                .setExpirationTime("60h")
-                .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
-            req.data = jwt;
-            res.status(201).send({ status: 201, message: jwt });
+            res.status(200).json({ rol: result[0].rol })
         } else {
             res.status(500).json({ message: "No se ha encontrado ningun usuario con esas credenciales" })
         }
