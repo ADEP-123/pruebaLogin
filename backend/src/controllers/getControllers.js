a808a2a19747c084908309c1dfbc7ce4a72d8e13
ADEP-123 <andep210399@gmail.com>
2023-09-10 10:05:32 -0500

    feat: :sparkles: creando token generator

--- backend/src/controllers/getControllers.js
+++ backend/src/controllers/getControllers.js
@@ -1,3 +1,8 @@
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import { Router } from 'express';
+import { SignJWT } from 'jose';
+
 import { getRolService } from "../services/getServices.js";
 
 export const getRolController = async (req, res, next) => {
@@ -5,7 +10,15 @@ export const getRolController = async (req, res, next) => {
     try {
         const result = await getRolService(user, pass);
         if (result.length != 0) {
-            res.status(200).json({ rol: result[0].rol })
+            const encoder = new TextEncoder();
+            const jwtconstructor = new SignJWT(Object.assign({}, Object.assign(result[0])));
+            const jwt = await jwtconstructor
+                .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+                .setIssuedAt()
+                .setExpirationTime("60h")
+                .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+            req.data = jwt;
+            res.status(201).send({ status: 201, message: jwt });
         } else {
             res.status(500).json({ message: "No se ha encontrado ningun usuario con esas credenciales" })
         }
